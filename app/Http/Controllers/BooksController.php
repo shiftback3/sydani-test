30b5e6bf78f982cc79d42af9f047c9c77bc0bba4
Ogbu Garuba <shiftback3@gmail.com>
2021-09-04 17:28:25 +0100

    done with backend CRUD

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,8 +3,129 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Books;
+use Illuminate\Support\Facades\Validator;
+use GuzzleHttp\Client;
 
 class BooksController extends Controller
 {
     //
+    public function getAllBooks() {
+        // logic to get all Bookss goes here
+    //     $books = Books::get()->toJson(JSON_PRETTY_PRINT);
+    // return response($books, 200);
+    
+    $client = new Client((['base_uri' => 'https://www.anapioficeandfire.com/api/']));
+       
+    $response = $client->request('GET', 'books');
+    // return response([
+    //     'status_code' => $response->getStatusCode(),
+    //      'status' => "success",
+    //      'data' => $response->getBody()->getContents() ]);
+     $output = json_decode($response->getBody()->getContents());
+    
+            $newdata = array();
+            $count = count($output);
+    for ($x = 0; $x < $count ; $x++) {
+       $newdata[] =  array(
+           "id" => $x + 1,
+           "name" => $output[$x]->name,
+           "isbn" => $output[$x]->isbn,
+           "authors" => $output[$x]->authors,
+           "number_of_pages" => $output[$x]->numberOfPages,
+           "publisher" => $output[$x]->publisher,
+           "country" => $output[$x]->country,
+           "release_date" => $output[$x]->released,
+        );
+       
+    }
+    
+    // return $newdata;
+     return response([
+        'status_code' => $response->getStatusCode(),
+         'status' => "success",
+         'data' => $newdata 
+        ]);
+    
+    
+      }
+  
+      public function createBook(Request $request) {
+        // logic to create a Books record goes here
+        $book = new Books;
+      $book->name = $request->name;
+      $book->isbn = $request->isbn;
+      $book->authors = $request->authors;
+      $book->number_of_pages = $request->number_of_pages;
+      $book->country = $request->country;
+      $book->publisher = $request->publisher;
+      $book->release_date = $request->release_date;
+      $book->save();
+
+      return response()->json([
+        "status_code" => 201,
+        "status" => "success",
+        "data" => $book
+      ], 201);
+      }
+  
+      public function getBook($id) {
+        // logic to get a Books record goes here
+
+        if (Books::where('id', $id)->exists()) {
+            $Book = Books::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($Book, 200);
+          } else {
+            return response()->json([
+              "message" => "Book not found"
+            ], 404);
+          }
+      }
+  
+      public function updateBook(Request $request, $id) {
+        // logic to update a Books record goes here
+        if (Books::where('id', $id)->exists()) {
+            $Book = Books::find($id);
+    
+            $Book->name = is_null($request->name) ? $Book->name : $request->name;
+            $Book->isbn = is_null($request->isbn) ? $Book->isbn : $request->isbn;
+            $Book->authors = is_null($request->authors) ? $Book->authors : $request->authors;
+            $Book->number_of_pages = is_null($request->number_of_pages) ? $Book->number_of_pages : $request->number_of_pages;
+            $Book->country = is_null($request->country) ? $Book->country : $request->country;
+            $Book->publisher = is_null($request->publisher) ? $Book->publisher : $request->publisher;
+            $Book->release_date = is_null($request->release_date) ? $Book->release_date : $request->release_date;
+           
+            $Book->save();
+    
+            return response()->json([
+                "status_code" => 200,
+                "status" => "success",
+                "message" => "The book was updated successfully",
+            ], 200);
+          } else {
+            return response()->json([
+              "message" => "Book not found"
+            ], 404);
+          }
+      }
+  
+      public function deleteBook ($id) {
+        // logic to delete a Books record goes here
+        if(Books::where('id', $id)->exists()) {
+            $Books = Books::find($id);
+            $Books->delete();
+    
+            return response()->json([
+                "status_code" => 200,
+                "status" => "success",
+                "message" => "The book was deleted successfully",
+                "data" => []
+            ], 202);
+          } else {
+            return response()->json([
+              "message" => "Book not found"
+            ], 404);
+          }
+        }
+      
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::group(['prefix' => '{v1}'], function () {
+Route::get('books', [BooksController::class, 'getAllBooks']);
+Route::get('books/{id}', [BooksController::class, 'getBook']);
+Route::post('books', [BooksController::class, 'createBook']);
+Route::put('books/{id}', [BooksController::class, 'updateBook']);
+Route::delete('books/{id}', [BooksController::class, 'deleteBook']);
 });
