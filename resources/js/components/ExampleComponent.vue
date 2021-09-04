955931cb79f56d472bedd94a0c80b94db5de8d14
Ogbu Garuba <shiftback3@gmail.com>
2021-09-04 18:01:38 +0100

    added vue to app

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>ISBN</th>
+        <th>Authors</th>
+      </tr>
+    </thead>
+    <tbody>
+      
+     
+    </tbody>
+  </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
